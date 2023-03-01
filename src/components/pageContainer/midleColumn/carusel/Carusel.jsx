4efcc883630766ead542a668929a80f3253eb996
Art <121219238+ArtCafe@users.noreply.github.com>
2023-03-01 20:11:34 +0200

    Update Carusel.jsx

--- src/components/pageContainer/midleColumn/carusel/Carusel.jsx
+++ src/components/pageContainer/midleColumn/carusel/Carusel.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {  useSelector } from "react-redux";
-import pasta from "../../../../img/pasta.jpg";
+//import pasta from "../../../../img/pasta.jpg";
 import 'w3-css/w3.css';
 
 function Carusel () {
@@ -26,7 +26,7 @@ if (!popularPosts){
   {popularPosts?.map((post, idx) => (
 
   <div className="w3-container w3-card w3-white w3-round w3-margin  w3-col m6 "     key={idx} style={{width:180}}  ><br/>
-    <img src={pasta} alt="Avatar" className="w3-left w3-circle w3-margin-right w3-green" style={{width:60}}/>
+    <img src="https://res.cloudinary.com/dxdafplzp/image/upload/v1676543424/avatars/orxwhcn5nx37icljbroz.jpg" alt="Avatar" className="w3-left w3-circle w3-margin-right w3-green" style={{width:60}}/>
 
     <h4>{post.title}</h4><br/>
     <hr className="w3-clear"/>
